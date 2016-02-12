10a58c2e9f21a56622b6be1c3133f7506a74997c
Rick Housley <bug@localhost.localdomain>
2016-02-11 23:09:04 -0500

    finished individual parsing

--- ged_parser.py
+++ ged_parser.py
@@ -1,25 +1,155 @@
-filename = 'rhousley_P02.ged'
+FILENAME = 'default_ged.ged'
 
-valid_tags = ['INDI', 'NAME', 'SEX', 'BIRT', 'DEAT', 'FAMC', 'FAMS', 'FAM', \
+VALID_TAGS = ['INDI', 'NAME', 'SEX', 'BIRT', 'DEAT', 'FAMC', 'FAMS', 'FAM', \
     'MARR', 'HUSB', 'WIFE', 'CHIL', 'DIV', 'DATE', 'HEAD', 'TRLR', 'NOTE']
 
-def main():
-    lines = [line.rstrip('\n\r') for line in open(filename)]
+class Gedline:
+    """Class for line of gedcom"""
 
-    for line in lines:
+    def __init__(self, line):
+        self.level = None
+        self.tag = None
+        self.xref = None
+        self.args = None
+
+        # Do parsing into level, tag, args, xref
         line_listified = line.split(' ',)
+        self.level = int(line_listified[0])
 
-        level = line_listified[0]
-        tag = line_listified[1]
-        args = line_listified[2:]
+        # Default tag format if level > 0
+        # <level-number> <tag> <args>
+        if (self.level > 0):
+            self.tag = line_listified[1]
+            self.args = line_listified[2:]
 
-        # Do printing
-        print line
-        print level
-        if (tag in valid_tags):
-            print tag
+        # Check for non default formats
+        if (self.level == 0):
+            # <level-number> <tag> <ignorable args>
+            if line_listified[1] in VALID_TAGS:
+                self.tag = line_listified[1]
+                self.args = line_listified[2:]
+                if self.args == []:
+                    self.args = None
+            # <level-number> <xref-id> <tag>
+            else:
+                self.xref = line_listified[1]
+                self.tag = line_listified[2]
+
+    def __str__(self):
+        return self.tag
+
+    def check_if_valid(self):
+        if (self.tag in VALID_TAGS):
+            return True
         else:
-            print "Invalid tag"
+            return False
+
+class Individual:
+    """Class for individual"""
+
+    def __init__(self, id):
+        self.id = id
+        self.name = None  # Name of individual
+        self.sex = None # Sex of individual (M or F)
+        self.birthdate = None # Birth date of individual
+        self.death = None # Date of death of individual
+        self.famc = None # Family where individual is a child
+        self.fams = None # Family where individual is spouse
+
+class Family:
+    """Class for family"""
+
+    def __init__(self):
+        pass
+
+def main():
+    gedlist = []
+    individuals = []
+    families = []
+
+    lines = [line.rstrip('\n\r') for line in open(FILENAME)]
+
+    for l in lines:
+        #print l
+        current_ged = Gedline(l)
+        gedlist.append(current_ged)
+
+    # Do invidual parsing
+    temp = None
+    for i, g in enumerate(gedlist):
+        if (g.tag == 'INDI'):
+            individuals.append(fill_individual(gedlist, i, g.xref))
+        if (g.tag == 'FAM'):
+            pass
+
+    for indiv in individuals:
+        print indiv.name, indiv.id
+
+def fill_individual(gedlist, index, xref):
+    indiv = Individual(xref)
+    endNotFound = True
+    date_type = None
+    for i, g in enumerate(gedlist[index+1:]):
+        if (g.level == 0):
+            break
+        if (g.tag == "NAME"):
+            indiv.name = g.args
+        if (g.tag == "SEX"):
+            indiv.sex = g.args[0]
+        if (g.tag == "BIRT"):
+            date_type = "BIRT"
+        if (g.tag == "DEAT"):
+            date_type = "DEAT"
+        if (g.tag == "FAMC"):
+            indiv.famc = g.args[0]
+        if (g.tag == "FAMS"):
+            indiv.fams = g.args[0]
+
+        # This assumes the following date tag corresponds to prev tag
+        if (g.tag == "DATE"):
+            if (date_type=="BIRT"):
+                indiv.birthdate = g.args
+                date_type = None
+            elif (date_type=="DEAT"):
+                indiv.death = g.args
+                date_type = None
+            else:
+                print "ERROR"
+
+    return indiv
+
+# def fill_family(gedlist, index, xref):
+#     family = Individual(xref)
+#     endNotFound = True
+#     date_type = None
+#     for i, g in enumerate(gedlist[index+1:]):
+#         if (g.level == 0):
+#             break
+#         if (g.tag == "NAME"):
+#             indiv.name = g.args
+#         if (g.tag == "SEX"):
+#             indiv.sex = g.args[0]
+#         if (g.tag == "BIRT"):
+#             date_type = "BIRT"
+#         if (g.tag == "DEAT"):
+#             date_type = "DEAT"
+#         if (g.tag == "FAMC"):
+#             indiv.famc = g.args[0]
+#         if (g.tag == "FAMS"):
+#             indiv.fams = g.args[0]
+#
+#         # This assumes the following date tag corresponds to prev tag
+#         if (g.tag == "DATE"):
+#             if (date_type=="BIRT"):
+#                 indiv.birthdate = g.args
+#                 date_type = None
+#             elif (date_type=="DEAT"):
+#                 indiv.death = g.args
+#                 date_type = None
+#             else:
+#                 print "ERROR"
+#
+#     return indiv
 
 
 if __name__ == '__main__':
