0357a7ee5dc21d62923c41980f9bc2b4f0669580
Bryan Gardner <gardner.bryan22@gmail.com>
2016-04-16 12:25:38 -0400

    implement and test US22 -- unique IDs

--- src/unit_tests.py
+++ src/unit_tests.py
@@ -15,7 +15,7 @@ from user_stories import dates_before_current, birth_before_marriage, \
     birth_before_marriage_of_parents, multiple_births_less_5, \
     no_sibling_marriage, no_marriage_to_decendants, \
     fewer_than_fifteen_siblings, male_last_names, \
-    sibling_spacing, correct_gender_for_role
+    sibling_spacing, correct_gender_for_role, unique_ids
 
 FAIL_DIR = "acceptance_files/fail/"
 PASS_DIR = "acceptance_files/pass/"
@@ -298,7 +298,6 @@ class TestParser(unittest.TestCase):
         else:
             print "!!fewer_than_fifteen_siblings acceptance file not found"
 
-
     def test_correct_gender_for_role(self):
         """ Unit test for correct_gender_for_role """
 
@@ -317,6 +316,25 @@ class TestParser(unittest.TestCase):
             self.assertFalse(correct_gender_for_role(individuals, families))
         else:
             print "!!correct_gender_for_role acceptance file not found"
+
+    def test_unique_ids(self):
+        """ Unit test for unique_ids """
+
+        acceptf = "unique_ids.ged"
+        fail_file = FAIL_DIR + acceptf
+        pass_file = PASS_DIR + acceptf
+
+        if os.path.exists(pass_file):
+            individuals, families = parse_ged(pass_file)
+            self.assertTrue(unique_ids(individuals, families))
+        else:
+            print "!!unique_ids acceptance file not found"
+
+        if os.path.exists(fail_file):
+            individuals, families = parse_ged(fail_file)
+            self.assertFalse(unique_ids(individuals, families))
+        else:
+            print "!!unique_ids acceptance file not found"
 
     def test_male_last_names(self):
         """ Unit test for male_last_names """
--- src/user_stories.py
+++ src/user_stories.py
@@ -45,6 +45,7 @@ def validation(individuals, families):
 
     # Sprint 4
     correct_gender_for_role(individuals, families)
+    unique_ids(individuals, families)
 
 
 def report(rtype, number, description, location):
@@ -675,6 +676,33 @@ def correct_gender_for_role(individuals, families):
             anom_location = [individual.uid, family.uid]
             report_anomaly(anom_type, anom_descrip, anom_location)
             return_flag = False
+    return return_flag
+
+
+def unique_ids(individuals, families):
+    """ US22 - All individual IDs and Family IDs should be unique """
+    error_type = "US22"
+    return_flag = True
+
+    individual_list = []
+    family_list = []
+
+    for individual in individuals:
+        if individual.uid in individual_list:
+            error_descrip = "Individual ID already exists"
+            error_location = [individual.uid]
+            report_error(error_type, error_descrip, error_location)
+            return_flag = False
+        else:
+            individual_list.append(individual.uid)
+    for family in families:
+        if family.uid in family_list:
+            error_descrip = "Family ID already exists"
+            error_location = [family.uid]
+            report_error(error_type, error_descrip, error_location)
+            return_flag = False
+        else:
+            family_list.append(family.uid)
     return return_flag
 
 
